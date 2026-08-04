1533aa338618a3798661bdac5a51bf5637338b45
Khuất Tuấn Anh <124482010+nstpvcpiooi@users.noreply.github.com>
2026-08-04 14:06:18 +0700

    github page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/introduction-to-probability/",
   plugins: [react()],
   resolve: {
     alias: {
